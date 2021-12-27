8088805abb0b5d8c56fcb84b25dba3c05ec472c2
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2021-12-27 16:39:57 +0100

    added hyperlinks for navbar items

--- routes/content.py
+++ routes/content.py
@@ -11,3 +11,13 @@ def home():
     sensors_data = [sensor.parse_sensors_data() for sensor in sensors_manager.get_sensors()]
 
     return render_template("index.html", sensors_data=sensors_data)
+
+
+@content_.route("/settings")
+def settings():
+    return "Settings"
+
+
+@content_.route("/graphs")
+def graphs():
+    return "Graphs"
