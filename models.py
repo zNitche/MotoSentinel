f1d5573d8981ca41485a47eed680e5547f2294aa
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-01-01 23:57:15 +0100

    fixed typo in models.py

--- models.py
+++ models.py
@@ -4,14 +4,14 @@ from __init__ import db
 class Gyro(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, nullable=False)
-    x_value = db.Column(db.Flaot, nullable=False)
-    y_value = db.Column(db.Flaot, nullable=False)
-    z_value = db.Column(db.Flaot, nullable=False)
+    x_value = db.Column(db.Float, nullable=False)
+    y_value = db.Column(db.Float, nullable=False)
+    z_value = db.Column(db.Float, nullable=False)
 
 
 class Accelerometer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, nullable=False)
-    x_value = db.Column(db.Flaot, nullable=False)
-    y_value = db.Column(db.Flaot, nullable=False)
-    z_value = db.Column(db.Flaot, nullable=False)
+    x_value = db.Column(db.Float, nullable=False)
+    y_value = db.Column(db.Float, nullable=False)
+    z_value = db.Column(db.Float, nullable=False)
