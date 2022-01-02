56635e10ac5a73468dddecc14f6a7e18bfbbaa13
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-01-02 15:22:15 +0100

    settings system refactoring

--- routes/settings.py
+++ routes/settings.py
@@ -5,15 +5,21 @@ from utils import settings_utils
 settings_ = Blueprint("settings", __name__, template_folder='template', static_folder='static')
 
 
-@settings_.route("/settings/graphs/set_time_range/<mode>/", methods=["GET"])
-def set_time_range(mode):
-    if request.args.get(mode):
-        refresh_rate = int(request.args.get(mode))
+@settings_.route("/settings/apply_settings", methods=["GET"])
+def apply_settings():
+    requests_dict = request.args.to_dict()
 
-        settings_data = settings_utils.load_settings()
+    if len(requests_dict.keys()) > 0:
+        for settings_mode in requests_dict:
+            settings_data = settings_utils.load_settings()
 
-        settings_data[mode] = refresh_rate
+            settings_mode_value = requests_dict[settings_mode]
 
-        settings_utils.save_settings(settings_data)
+            if settings_mode_value.isnumeric():
+                settings_mode_value = int(settings_mode_value)
+
+            settings_data[settings_mode] = settings_mode_value
+
+            settings_utils.save_settings(settings_data)
 
     return redirect(url_for("content.settings"))
