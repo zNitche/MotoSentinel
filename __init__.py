43721fe3f61851134c91a40f7c120991a249377a
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-01-02 14:16:50 +0100

    main app factory refactoring

--- __init__.py
+++ __init__.py
@@ -3,6 +3,20 @@ import os
 from models import db
 
 
+def create_background_managers(app):
+    from background_managers.sensors_manager import SensorManager
+    from background_managers.db_manager import DBManager
+
+    sensors_manager = SensorManager()
+    sensors_manager.start()
+
+    db_manager = DBManager(app, db, sensors_manager)
+    db_manager.start()
+
+    app.config["DB_MANAGER"] = db_manager
+    app.config["SENSORS_MANAGER"] = sensors_manager
+
+
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object("app_config.AppConfig")
@@ -10,20 +24,10 @@ def create_app():
 
     db.init_app(app)
 
-    from background_managers.sensors_manager import SensorManager
-    from background_managers.db_manager import DBManager
-
-    sensors_manager = SensorManager()
-    sensors_manager.start()
-
     with app.app_context():
         db.create_all()
 
-        db_manager = DBManager(app, db, sensors_manager)
-        db_manager.start()
-
-        app.config["DB_MANAGER"] = db_manager
-        app.config["SENSORS_MANAGER"] = sensors_manager
+        create_background_managers(app)
 
         from routes import content, errors, sensors_api, settings
 
