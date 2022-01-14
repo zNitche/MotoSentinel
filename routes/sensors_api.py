c80e9491d176c33ab5a45c0a6f7ec16d42c042fc
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-01-14 17:56:33 +0100

    added sensors api endopint for getting names of all used sensors

--- routes/sensors_api.py
+++ routes/sensors_api.py
@@ -5,10 +5,19 @@ from app_config import AppConfig
 sensors_ = Blueprint("sensors", __name__, template_folder='template', static_folder='static')
 
 
-@sensors_.route(f"/api/sensors/<sensor_name>")
+@sensors_.route("/api/sensors/<sensor_name>")
 def sensor_data(sensor_name):
     sensors_manager = current_app.config[AppConfig.SENSORS_MANAGER_CONFIG_KEY_NAME]
 
     data = sensors_manager.get_sensor_parsed_data_by_sensor_name(sensor_name)
 
     return jsonify(data)
+
+
+@sensors_.route("/api/sensors")
+def sensors_list():
+    sensors_manager = current_app.config[AppConfig.SENSORS_MANAGER_CONFIG_KEY_NAME]
+
+    sensors = [sensor.name for sensor in sensors_manager.get_sensors()]
+
+    return jsonify(sensors)
