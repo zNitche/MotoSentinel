3f7d6de5a1851dea8c8f762544f161136cf80895
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-01-15 15:17:06 +0100

    added api endpoint for getting sensors data from db

--- routes/sensors_api.py
+++ routes/sensors_api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, current_app
 from app_config import AppConfig
-
+from utils import db_utils
+from sensors.sensors_config import SensorsConfig
 
 sensors_ = Blueprint("sensors", __name__, template_folder='template', static_folder='static')
 
@@ -21,3 +22,19 @@ def sensors_list():
     sensors = [sensor.name for sensor in sensors_manager.get_sensors()]
 
     return jsonify(sensors)
+
+
+@sensors_.route("/api/sensors_data/<sensor_name>")
+def sensors_db_data(sensor_name):
+    sensor_db_data = {}
+
+    if sensor_name == SensorsConfig.ACCELEROMETER_SENSOR_NAME:
+        sensor_db_data = db_utils.get_acceleration_data()
+
+    elif sensor_name == SensorsConfig.GYRO_SENSOR_NAME:
+        sensor_db_data = db_utils.get_gyro_data()
+
+    elif sensor_name == SensorsConfig.TEMP_SENSOR_NAME:
+        sensor_db_data = db_utils.get_temp_data()
+
+    return jsonify(sensor_db_data)
