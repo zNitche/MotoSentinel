c3e0fa4f8f92fdf3133cede31aef2f9093f3c03f
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-02-26 15:05:05 +0100

    moved data files (db etc.) to data directory

--- app_config.py
+++ app_config.py
@@ -10,13 +10,15 @@ class AppConfig:
     SENSORS_MANAGER_CONFIG_KEY_NAME = "SENSORS_MANAGER"
     DB_MANAGER_CONFIG_KEY_NAME = "DB_MANAGER"
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    DATA_PATH = "data"
+
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DATA_PATH}/database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     DO_LOGS = True
-    LOGS_PATH = os.path.join(CURRENT_DIR, "log.txt")
+    LOGS_PATH = os.path.join(CURRENT_DIR, DATA_PATH, "log.txt")
 
-    SETTINGS_PATH = os.path.join(CURRENT_DIR, "settings.json")
+    SETTINGS_PATH = os.path.join(CURRENT_DIR, DATA_PATH, "settings.json")
 
     PNG_ENCODE_STRING = "data:image/png;base64,"
     GRAPH_DPI_RESOLUTION = 150
